d055ad6357c32c55422d7ab1647bc87dfa691bde
Cynthia Wang <CynthiaWang7979@hotmail.com>
2021-07-28 01:13:51 +0800

    Completed checkForWinner function

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -55,8 +55,15 @@ struct ContentView: View {
     }
     
     private func checkForWinner() {
-        if moves[0] != "" && moves[0] == moves[1] && moves[1] == moves[2] {
-            winMessage = "\(moves[0]) is the winner!"
+        checkLine(a: 0, b: 1, c: 2)  // row 1
+        checkLine(a: 3, b: 4, c: 5)  // row 2
+        checkLine(a: 6, b: 7, c: 8)  // row 3
+        
+    }
+    
+    private func checkLine(a: Int, b: Int, c: Int) {
+        if moves[a] != "" && moves[a] == moves[b] && moves[b] == moves[c] {
+            winMessage = "\(moves[a]) is the winner!"
             gameOver = true
         }
     }
